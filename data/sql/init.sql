dd3bce3e12e3e727d16a467685e6f974ff6e3f29
Hun Jae Lee <hunjael@gmail.com>
2020-04-19 18:27:12 -0400

    :gear: fix init.sql

--- data/sql/init.sql
+++ data/sql/init.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE `hackathonmentors` IF NOT EXISTS;
-CREATE USER 'hackathonmentors_user'@'%' IDENTIFIED BY 'hackathonmentors_pass';
-GRANE ALL PRIVILEGES ON `hackathonmentors`.* to 'hackathonmentors_user'@'%';
+CREATE DATABASE IF NOT EXISTS `hackathonmentors`;
+CREATE USER IF NOT EXISTS 'hackathonmentors_user'@'%' IDENTIFIED BY 'hackathonmentors_pass';
+GRANT ALL PRIVILEGES ON `hackathonmentors`.* to 'hackathonmentors_user'@'%';
 FLUSH PRIVILEGES;
