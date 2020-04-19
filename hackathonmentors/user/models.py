5241d0c65b731af00e613545571b4e4c67e5432e
Hun Jae Lee <hunjael@gmail.com>
2020-04-19 18:30:14 -0400

    :raising_hand: create model CustomUser

--- hackathonmentors/user/models.py
+++ hackathonmentors/user/models.py
@@ -2,6 +2,9 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 """
-TODO: Override Django's basic AbstractUser class if needed
+Overrides Django's basic AbstractUser class if needed
 Reference: https://github.com/django/django/blob/master/django/contrib/auth/models.py
 """
+
+class CustomUser(AbstractUser):
+    pass
