8bb43efac798bcf8035afa2a5bdd18e67279b972
Hun Jae Lee <hunjael@gmail.com>
2020-04-19 18:35:52 -0400

    :fish_cake: create model & migration for Hackathon events

--- hackathonmentors/hackathon/models.py
+++ hackathonmentors/hackathon/models.py
@@ -1,2 +1,15 @@
 from django.db import models
 
+
+class Hackathon(models.Model):
+    name = models.CharField(max_length=64)
+    location = models.CharField(max_length=64)
+    starts = models.DateTimeField()
+    ends = models.DateTimeField()
+
+    added_by = models.ForeignKey('user.CustomUser', on_delete=models.SET(1))
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        db_table = 'hackathons'
