39fd494bf391d6ed5f3c5f6d88162a946bd45d44
Hun Jae Lee <hunjael@gmail.com>
2020-04-19 17:32:35 -0400

    :fish_cake: fix template extending

--- hackathonmentors/user/models.py
+++ hackathonmentors/user/models.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+"""
+TODO: Override Django's basic AbstractUser class if needed
+Reference: https://github.com/django/django/blob/master/django/contrib/auth/models.py
+"""
