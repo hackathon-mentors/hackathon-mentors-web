32ce8afebf5ce24b69d1e7148ecea7ecff5963a2
Hun Jae Lee <hunjael@gmail.com>
2020-04-19 18:29:56 -0400

    :raising_hand: override allauth.views to customize

---
+++ hackathonmentors/user/urls.py
@@ -0,0 +1,32 @@
+from django.urls import path, re_path
+
+from . import views
+
+
+urlpatterns = [
+    path("signup/", views.HMSignupView.as_view(), name="account_signup"),
+    path("login/", views.HMLoginView.as_view(), name="account_login"),
+    path("logout/", views.HMLogoutView.as_view(), name="account_logout"),
+    path("password/change/", views.HMPasswordChangeView.as_view(),
+         name="account_change_password"),
+    path("password/set/", views.HMPasswordSetView.as_view(), name="account_set_password"),
+    path("inactive/", views.HMAccountInactiveView.as_view(), name="account_inactive"),
+
+    # E-mail
+    path("email/", views.HMEmailView.as_view(), name="account_email"),
+    path("confirm-email/", views.HMEmailVerificationSentView.as_view(),
+         name="account_email_verification_sent"),
+    re_path(r"^confirm-email/(?P<key>[-:\w]+)/$", views.HMConfirmEmailView.as_view(),
+            name="account_confirm_email"),
+
+    # password reset
+    path("password/reset/", views.HMPasswordResetView.as_view(),
+         name="account_reset_password"),
+    path("password/reset/done/", views.HMPasswordResetDoneView.as_view(),
+         name="account_reset_password_done"),
+    re_path(r"^password/reset/key/(?P<uidb36>[0-9A-Za-z]+)-(?P<key>.+)/$",
+            views.HMPasswordResetFromKeyView.as_view(),
+            name="account_reset_password_from_key"),
+    path("password/reset/key/done/", views.HMPasswordResetFromKeyDoneView.as_view(),
+         name="account_reset_password_from_key_done"),
+]
--- hackathonmentors/user/views.py
+++ hackathonmentors/user/views.py
@@ -1,3 +1,61 @@
 from django.shortcuts import render
 
-# Create your views here.
+from allauth.account import views as allauth
+
+"""
+Overrides django-allauth's view implementations.
+https://github.com/pennersr/django-allauth/blob/master/allauth/account/views.py
+"""
+
+
+class HMLoginView(allauth.LoginView):
+    pass
+
+
+class HMSignupView(allauth.SignupView):
+    template_name = 'user/signup.html'
+
+
+class HMConfirmEmailView(allauth.ConfirmEmailView):
+    pass
+
+
+class HMEmailView(allauth.EmailView):
+    pass
+
+
+class HMPasswordChangeView(allauth.PasswordChangeView):
+    pass
+
+
+class HMPasswordSetView(allauth.PasswordSetView):
+    pass
+
+
+class HMPasswordResetView(allauth.PasswordResetView):
+    pass
+
+
+class HMPasswordResetDoneView(allauth.PasswordResetDoneView):
+    pass
+
+
+class HMPasswordResetFromKeyView(allauth.PasswordResetFromKeyView):
+    pass
+
+
+class HMPasswordResetFromKeyDoneView(allauth.PasswordResetFromKeyDoneView):
+    pass
+
+
+class HMLogoutView(allauth.LogoutView):
+    pass
+
+
+class HMAccountInactiveView(allauth.AccountInactiveView):
+    pass
+
+
+class HMEmailVerificationSentView(allauth.EmailVerificationSentView):
+    pass
+
