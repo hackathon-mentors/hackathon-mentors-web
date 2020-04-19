74c90014c8602356b533aa9ff076862eed10410c
Hun Jae Lee <hunjael@gmail.com>
2020-04-19 19:32:15 -0400

    :gear: wildcard ALLOWED_HOSTS

--- hackathonmentors/hackathonmentors/settings.py
+++ hackathonmentors/hackathonmentors/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", 1)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 if DEBUG:
     EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
