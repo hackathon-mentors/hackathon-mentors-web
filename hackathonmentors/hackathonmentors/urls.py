d100511b6d9e27e1c1cda53440ec8960590abe59
Hun Jae Lee <hunjael@gmail.com>
2020-04-19 14:11:57 -0400

    :rainbow: start basic template structure

--- hackathonmentors/hackathonmentors/urls.py
+++ hackathonmentors/hackathonmentors/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
+from django.contrib.staticfiles.storage import staticfiles_storage
 from django.urls import include, path
+from django.views.generic.base import RedirectView
 
 from hackathonmentors import views
 
@@ -7,4 +9,5 @@ urlpatterns = [
     path('', views.BaseView.as_view(template_name="index.html")),
 
     path('admin/', admin.site.urls),
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('images/favicon.ico'))),
 ]
