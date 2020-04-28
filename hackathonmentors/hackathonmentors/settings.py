28566290c3a270c857f7243751cae40fa9c2965e
Hun Jae Lee <hunjael@gmail.com>
2020-04-28 17:35:34 -0400

    :gear: configure STATIC_ROOT

--- hackathonmentors/hackathonmentors/settings.py
+++ hackathonmentors/hackathonmentors/settings.py
@@ -156,4 +156,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = '/build/hackathonmentors/static'
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "hackathonmentors", "static"),
+]
