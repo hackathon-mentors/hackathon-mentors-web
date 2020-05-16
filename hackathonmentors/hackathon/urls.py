b8d196b2041cb2e05d7411d1928b7341b19a96d5
Hun Jae Lee <hunjael@gmail.com>
2020-05-16 17:35:56 -0400

    :sparkles: basic list/add for hackathon events (no validations)

--- hackathonmentors/hackathon/urls.py
+++ hackathonmentors/hackathon/urls.py
@@ -4,4 +4,6 @@ from hackathon import views
 
 urlpatterns = [
     path('', views.HackathonListView.as_view(), name='hackathon_list'),
+    path('new/', views.HackathonCreateView.as_view(), name='hackathon_add'),
+    path('<slug:slug>/', views.HackathonDetailsView.as_view(), name='hackathon_view')
 ]
--- hackathonmentors/hackathon/views.py
+++ hackathonmentors/hackathon/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from django.views.generic import ListView, DetailView
 
 from hackathonmentors.views import HackathonMentorsMixin
 from hackathon.models import Hackathon
+
+from slugify import slugify
 
 
 class HackathonListView(HackathonMentorsMixin, ListView):
@@ -13,3 +18,45 @@ class HackathonListView(HackathonMentorsMixin, ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         return context
+
+
+class HackathonDetailsView(HackathonMentorsMixin, DetailView):
+    model = Hackathon
+    template_name = "hackathon/view.html"
+    context_object_name = 'hackathon'
+
+
+class HackathonCreateView(HackathonMentorsMixin, CreateView):
+    model = Hackathon
+    fields = [
+        "name",
+        "location",
+        "starts",
+        "ends",
+    ]
+    template_name = "hackathon/add.html"
+
+    def validate(self, form):
+        pass
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        # TODO: add duplicate name validation
+
+        self.object.slug = self.generate_slug()
+        self.object.added_by = self.request.user
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('hackathon_view', args=[self.object.slug])
+
+    def generate_slug(self):
+        slug = slugify(self.object.name)
+        if self.model.objects.filter(slug=slug).exists():
+            # TODO: handle duplicate slug
+            pass
+        return slug
+
+
+class HackathonDeleteView(HackathonMentorsMixin, CreateView):
+    model = Hackathon
