81a77890fe456917f01f5c3636f20600d5cdcbc6
Hun Jae Lee <hunjael@gmail.com>
2020-06-28 13:52:28 -0400

    :eyes: register our models to admin module

--- hackathonmentors/mentor/admin.py
+++ hackathonmentors/mentor/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from hackathon.models import Hackathon
+from mentor.models import Mentor
+from user.models import CustomUser
 
-# Register your models here.
+admin.site.register(Hackathon)
+admin.site.register(Mentor)
+admin.site.register(CustomUser)
