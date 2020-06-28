3613b9efa7d0d8863aa576bacf3193dbcd0b3dfa
Hun Jae Lee <hunjael@gmail.com>
2020-06-28 14:02:23 -0400

    :wave: lookup user by username=slug to grab 'em by the mentor

--- hackathonmentors/mentor/views.py
+++ hackathonmentors/mentor/views.py
@@ -4,6 +4,8 @@ from django.views.generic import ListView, DetailView
 
 from hackathonmentors.views import HackathonMentorsMixin
 from mentor.models import Mentor
+from user.models import CustomUser as User
+
 
 class MentorListView(HackathonMentorsMixin, ListView):
     model = Mentor
@@ -20,3 +22,17 @@ class MentorDetailsView(HackathonMentorsMixin, DetailView):
     model = Mentor
     template_name = "mentor/view.html"
     context_object_name = 'mentor'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+    
+    def get_object(self, queryset=None):
+        slug = self.kwargs.get(self.slug_url_kwarg)
+        user = User.objects.filter(username=slug)
+        if user:
+            user = user.first()
+            mentor = Mentor.objects.filter(user_id=user.id)
+            print(mentor)
+        
+        return mentor
