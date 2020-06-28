813c6eaf3e8f0cd592b08fc498052798f05768ae
Hun Jae Lee <hunjael@gmail.com>
2020-06-28 14:26:08 -0400

    :mask: create generic 404

--- hackathonmentors/hackathonmentors/views.py
+++ hackathonmentors/hackathonmentors/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.template import RequestContext
 
 from django.views.generic.base import TemplateView
 
@@ -9,3 +10,10 @@ class HackathonMentorsMixin(object):
 
 class BaseView(HackathonMentorsMixin, TemplateView):
     pass
+
+
+def handler404(request, *args, **argv):
+    response = render('404.html', {}, context_instance=RequestContext(request))
+    response.status_code = 404
+    return response
+
