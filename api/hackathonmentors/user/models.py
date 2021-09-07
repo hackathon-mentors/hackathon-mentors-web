8d3b0aede5d2c2d770fccc841774a6e5711b42f3
Hun Jae Lee <hunjael@gmail.com>
2021-09-07 05:01:23 -0400

    create model Skill for each skills and relation model UserSkill to map user-skill

--- api/hackathonmentors/user/models.py
+++ api/hackathonmentors/user/models.py
@@ -11,3 +11,13 @@ class CustomUser(AbstractUser):
 
     class Meta:
         db_table = 'users'
+
+
+class Skill(models.Model):
+    name = models.CharField(max_length=32)
+
+
+class UserSkill(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    skill = models.ForeignKey(Skill, on_delete=models.CASCADE)
+    competency = models.PositiveSmallIntegerField()
