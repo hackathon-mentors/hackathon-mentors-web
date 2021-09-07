3723c6bf4a39f9642209e1ad6c10fd95e117147b
Hun Jae Lee <hunjael@gmail.com>
2021-09-07 12:46:46 -0400

    use FormBuilder & {% crispy form %} templatetag for buildling bootstrap-enabled CrispyForm

--- api/hackathonmentors/user/forms.py
+++ api/hackathonmentors/user/forms.py
@@ -4,6 +4,9 @@ from user.models import CustomUser as User
 
 from django.utils.translation import gettext_lazy as _
 
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Row, Column, Field
+
 
 class HMUserSignupForm(SignupForm):
     # https://github.com/pennersr/django-allauth/blob/master/allauth/account/forms.py#L266
@@ -26,3 +29,13 @@ class HMUserEditForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'username']
+
+    helper = FormHelper()
+    helper.form_class = 'form-group'
+    helper.layout = Layout(
+        Row(
+            Column(Field('first_name'), css_class="col-6"),
+            Column(Field('last_name'), css_class="col-6"),
+        ),
+        Field('username', css_class='form-control mb-3'),
+    )
