b6b2d08f2b652cb2ed842a9de5bb2cd16fb4deee
Hun Jae Lee <hunjael@gmail.com>
2021-09-14 22:10:18 -0400

    :broom: each app's admin.py should have its models

--- api/hackathonmentors/mentor/admin.py
+++ api/hackathonmentors/mentor/admin.py
@@ -1,8 +1,4 @@
 from django.contrib import admin
-from hackathon.models import Hackathon
 from mentor.models import Mentor
-from user.models import CustomUser
 
-admin.site.register(Hackathon)
 admin.site.register(Mentor)
-admin.site.register(CustomUser)
--- api/hackathonmentors/user/admin.py
+++ api/hackathonmentors/user/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from user.models import CustomUser
 # Register your models here.
+
+admin.site.register(CustomUser)
