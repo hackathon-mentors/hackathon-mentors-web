e1a47eea62fd16d11870b190b168f6526007e803
Hun Jae Lee <hunjael@gmail.com>
2021-09-14 22:07:13 -0400

    :sparkles: give `__str__()` to the models

--- api/hackathonmentors/user/skill/models.py
+++ api/hackathonmentors/user/skill/models.py
@@ -6,8 +6,14 @@ from user.models import CustomUser as User
 class Skill(models.Model):
     name = models.CharField(max_length=32)
 
+    def __str__(self):
+        return self.name
+
 
 class UserSkill(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     skill = models.ForeignKey(Skill, on_delete=models.CASCADE)
     proficiency = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return f"{self.user}, {self.skill} ({self.proficiency})"
