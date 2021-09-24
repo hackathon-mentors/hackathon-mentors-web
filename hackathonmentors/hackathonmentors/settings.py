c091a8f0ede54484e4326433a8258dab1ecc7e19
Hun Jae Lee <hunjael@gmail.com>
2021-09-23 22:17:02 -0400

    :arrow_heading_up: update STATIC_URL

--- hackathonmentors/hackathonmentors/settings.py
+++ hackathonmentors/hackathonmentors/settings.py
@@ -192,8 +192,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = '/build/hackathonmentors/static'
-STATIC_URL = '/static/' if DEBUG else 'http://storage.googleapis.com/hm-staging/web/static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "hackathonmentors", "static"),
