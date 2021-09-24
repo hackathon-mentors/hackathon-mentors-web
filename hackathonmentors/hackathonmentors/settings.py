f55a8265e32f77c8ea3d226547eb7f132d8edbe4
Hun Jae Lee <hunjael@gmail.com>
2021-09-23 22:38:19 -0400

    :boom: goddamn STATIC

--- hackathonmentors/hackathonmentors/settings.py
+++ hackathonmentors/hackathonmentors/settings.py
@@ -196,17 +196,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'hackathonmentors', 'static')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'hackathonmentors', 'static'),
 )
 
 # Simplified static file serving.
