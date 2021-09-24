47929fe2cb97222d81cfebe70f688a6f4d808337
Hun Jae Lee <hunjael@gmail.com>
2021-09-23 22:19:08 -0400

    :arrow_heading_up: config heroku stuff for static

--- hackathonmentors/hackathonmentors/settings.py
+++ hackathonmentors/hackathonmentors/settings.py
@@ -192,8 +192,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "hackathonmentors", "static"),
-]
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
